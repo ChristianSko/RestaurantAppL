2e32e820b35eba2f9ea677ba832775d8b1c38090
Christian Skorobogatow <christiansko@icloud.com>
2021-01-25 11:37:22 +0100

    Initial Commit

---
+++ RestaurantAppL/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RestaurantAppL
+//
+//  Created by Christian Skorobogatow on 25/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RestaurantAppL/RestaurantAppLApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantAppLApp.swift
+//  RestaurantAppL
+//
+//  Created by Christian Skorobogatow on 25/1/21.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantAppLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
